2e98c73156d1fcabf3e22e5b07b93b056815bdaf
Stephane Zermatten <szermatt@gmx.net>
2020-06-14 13:14:13 +0300

    hlts-test: first test, with a basic highlight scenario.

--- test/hlts-test.el
+++ test/hlts-test.el
@@ -1,3 +1,15 @@
 ;;; hlts-test.el --- Tests for hlts
 
+(ert-deftest test-hlts-highlight ()
+  (test-hlts-env
+   (insert "(let ((hello)) (setq hello 1))")
+   (goto-char (point-min))
+   (search-forward "hello")
+   (goto-char (match-beginning 0))
+   (ert-run-idle-timers)
+   (should
+    (equal
+     (test-hlts-text-with-face (point-min) (point-max) 'hlts-face)
+     "(let (([hello])) (setq [hello] 1))"))))
+
 ;;; hlts-test.el ends here
--- test/test-helper.el
+++ test/test-helper.el
@@ -1,3 +1,53 @@
 ;;; test-helper.el --- Helpers for hlts-test.el
 
+(require 'hlts)
+(require 'ert)
+(require 'ert-x)
+
+(defmacro test-hlts-env (&rest body)
+  "Run BODY within a test environment.
+
+This macro overwrites relevant flags and options, creates a temp
+buffer in emacs-lisp mode and enables the hlts minor mode."
+  `(progn ;;save-window-excursion
+     (let ((hlts-idle-timeout 0.0)
+           (hlts-disable-for-faces '(font-lock-string-face))
+           (hlts-overlay-priority 0)
+           (emacs-lisp-mode-hook nil))
+       (ert-with-test-buffer ()
+         (display-buffer (current-buffer))
+         (emacs-lisp-mode)
+         (hlts-mode 1)
+         ,@body))))
+  
+(defun test-hlts-text-with-face (start end face)
+  "Return text region from START and END with FACE highlighted.
+
+The region of the text on which FACE is active are put withing brackets
+in the text that's returned."
+  (let ((result nil)
+        (active nil)
+        (last-pos start))
+    (save-excursion
+      (goto-char start)
+      (while (< (point) end)
+        (cond
+         ((and (not active)
+               (memq face (face-at-point nil t)))
+          (push (buffer-substring-no-properties last-pos (point)) result)
+          (push "[" result)
+          (setq last-pos (point))
+          (setq active t))
+         ((and active
+               (not (memq face (face-at-point nil t))))
+          (push (buffer-substring-no-properties last-pos (point)) result)
+          (push "]" result)
+          (setq last-pos (point))
+          (setq active nil)))
+        (goto-char (next-char-property-change (point) end)))
+      (push (buffer-substring-no-properties last-pos (point)) result)
+      (when active (push "]" result)))
+    (apply 'concat (nreverse result))))
+
+
 ;;; test-helper.el ends here
